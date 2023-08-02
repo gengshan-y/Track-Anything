bfa0aab1df0c2022d0201ca2978cb6658a1d86eb
Neehar Peri <neeharperi@users.noreply.github.com>
2023-08-02 18:55:36 -0400

    Update interact_tools.py

--- tools/interact_tools.py
+++ tools/interact_tools.py
@@ -1,5 +1,5 @@
 import time
-import torch
+import os
 import cv2
 from PIL import Image, ImageDraw, ImageOps
 import numpy as np
@@ -9,10 +9,13 @@ import matplotlib.pyplot as plt
 import PIL
 from .mask_painter import mask_painter as mask_painter2
 from .base_segmenter import BaseSegmenter
+import torch
+from groundingdino.util.inference import load_model, load_image, predict, annotate
 from .painter import mask_painter, point_painter
 import os
 import requests
 import sys 
+import uuid
 
 
 mask_color = 3
@@ -26,17 +29,19 @@ point_radius = 15
 contour_color = 2
 contour_width = 5
 
-
+gdino_config = "./preprocess/third_party/GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py"
+                 
 class SamControler():
-    def __init__(self, SAM_checkpoint, model_type, device):
+    def __init__(self, SAM_checkpoint, gdino_checkpoint, model_type, device):
         '''
         initialize sam controler
         '''
 
-    
         self.sam_controler = BaseSegmenter(SAM_checkpoint, model_type, device)
+        self.gdino = load_model(gdino_config, gdino_checkpoint)
+
+        self.device = device
         
-    
     # def seg_again(self, image: np.ndarray):
     #     '''
     #     it is used when interact in video
@@ -87,6 +92,56 @@ class SamControler():
         painted_image = Image.fromarray(painted_image)
         
         return mask, logit, painted_image
+
+    def extract_bbox(self, model, img_path, text_prompt, box_threshold, text_threshold):
+        image_source, image = load_image(img_path)
+        H, W = image_source.shape[0], image_source.shape[1]
+        
+        boxes, logits, phrases = predict(
+            model=model,
+            image=image,
+            caption=text_prompt,
+            box_threshold=box_threshold,
+            text_threshold=text_threshold
+        )
+
+        boxes = boxes * torch.Tensor([W, H, W, H]).repeat(repeats=(boxes.shape[0], 1))
+
+        # from xywh to xyxy
+        boxes[:, :2] -= boxes[:, 2:] / 2
+        boxes[:, 2:] += boxes[:, :2]
+
+        boxes = boxes.type("torch.IntTensor")
+
+        return boxes
+
+    def text_prompt(self, image: np.ndarray, text_prompt:str, box_threshold=0.35, text_threshold=0.25, multimask=True, mask_color=3):
+        '''
+        it is used in first frame in video
+        return: mask, logit, painted image(mask+point)
+        '''
+        # self.sam_controler.set_image(image)
+
+        tmp_file = "./preprocess/third_party/Track-Anything/tmp/" + str(uuid.uuid4()) + ".png"
+        
+        cv2.imwrite(tmp_file, cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+        boxes = self.extract_bbox(self.gdino, tmp_file, text_prompt, box_threshold, text_threshold)
+        
+        image = cv2.cvtColor(cv2.imread(tmp_file), cv2.COLOR_BGR2RGB)
+
+        prompts = {"boxes" : boxes.to(self.device), "shape" : image.shape[:2]}
+        masks, scores, logits = self.sam_controler.predict(prompts, 'bbox', multimask)
+        
+
+        masks = masks.cpu().numpy()
+        scores = scores.cpu().numpy()
+        logits = logits.cpu().numpy()
+        
+        mask, logit = masks[np.argmax(scores)], logits[np.argmax(scores), :, :]
+        painted_image = mask_painter(image, mask[np.argmax(scores)].astype('uint8'), mask_color, mask_alpha, contour_color, contour_width)
+        painted_image = Image.fromarray(painted_image)
+        
+        return mask[np.argmax(scores)], logit, painted_image
     
     # def interact_loop(self, image:np.ndarray, same: bool, points:np.ndarray, labels: np.ndarray, logits: np.ndarray=None, multimask=True):
     #     origal_image = self.sam_controler.orignal_image
